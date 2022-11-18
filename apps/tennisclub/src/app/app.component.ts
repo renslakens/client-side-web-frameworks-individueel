810591df11229933bc1a5fec8e7b6f5b68d2181f
Rens Lakens <rens@lakens.org>
2022-11-18 17:21:19 +0100

    Initial commit

---
+++ apps/tennisclub/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'clientside-web-frameworks-individueel-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'tennisclub';
+}
